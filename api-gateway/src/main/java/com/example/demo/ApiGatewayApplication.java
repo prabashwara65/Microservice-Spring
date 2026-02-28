9374aee108e5bec43d00acce1db24bc300bf28ae
Priyanka Prabashwara <prabashwara65@gmail.com>
2026-02-28 11:54:18 +0530

    fixed all pom files

--- api-gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
